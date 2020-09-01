728278a120dc8d272d8dd05621d8bcceecd54572
Zach Tuttle <tuttle_zach@icloud.com>
2020-08-31 18:57:04 -0700

    Minor updates.

--- lib/mittens_ui/widgets/button.rb
+++ lib/mittens_ui/widgets/button.rb
@@ -7,7 +7,18 @@ module MittensUi
           layout        = options[:layout]
           window        = options[:window]
       
+          margin_top = options.dig(:top).nil? ? nil : options.dig(:top)
+          margin_bottom = options.dig(:bottom).nil? ? nil : options.dig(:bottom)
+
           button = Gtk::Button.new(label: button_title)
+
+          unless margin_top.nil?
+            button.set_margin_top(margin_top)
+          end
+
+          unless margin_bottom.nil?
+            button.set_margin_bottom(margin_top)
+          end
          
           if layout[:grid]
             MittensUi::Layouts::Grid.attach(button, layout)
---
+++ lib/mittens_ui/widgets/list_box.rb
@@ -0,0 +1,12 @@
+module MittensUi
+  module Widgets
+    class ListBox
+      class << self
+        def init(options, block = Proc.new)
+          layout = options[:layout]
+          items = options[:items]
+        end
+      end
+    end
+  end
+end
