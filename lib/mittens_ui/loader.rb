cd586d9b1df47814f7eba7c64dca293815e820a8
Zach Tuttle <tuttle_zach@icloud.com>
2021-08-06 21:47:02 -0700

    adding loader back.

--- lib/mittens_ui/loader.rb
+++ lib/mittens_ui/loader.rb
@@ -1,37 +1,35 @@
 require_relative "./core"
 
 module MittensUi
-  module Widgets
-    class Loader < Core
-      def initialize(options={})
-        @spinner = Gtk::Spinner.new
+  class Loader < Core
+    def initialize(options={})
+      @spinner = Gtk::Spinner.new
 
-        @processing = false
+      @processing = false
 
-        $vertical_box.pack_end(@spinner)
+      $vertical_box.pack_end(@spinner)
 
-        super(@spinner, options)
+      super(@spinner, options)
 
-        self.hide
-      end
-
-      def start(&block)
-        return if @processing
-
-        return if @worker_thread && @worker_thread.alive?
-
-        self.show
-
-        @spinner.start
-
-        @worker_thread = Thread.new { yield; self.remove; @processing = true }
-      end
-
-      def render
-        $vertical_box.pack_end(@spinner)
-        return self
-      end
-
+      self.hide
     end
+
+    def start(&block)
+      return if @processing
+
+      return if @worker_thread && @worker_thread.alive?
+
+      self.show
+
+      @spinner.start
+
+      @worker_thread = Thread.new { yield; self.remove; @processing = true }
+    end
+
+    def render
+      $vertical_box.pack_end(@spinner)
+      return self
+    end
+
   end
 end
